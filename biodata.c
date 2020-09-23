30dd944bf8c259aec2e23ac791be9746849d56d2
Your Name <you@example.com>
2020-09-23 12:48:23 +0700

    initial commit

---
+++ biodata.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() 
+{
+	printf("Nama lengkap\t: Rizqy Ahsana Putri\n");
+	printf("Nama panggilan\t: Putri\n");
+	printf("TTL\t\t: Gresik, 1 Maret 2001\n");
+	printf("Jenis Kelamin\t: Perempuan\n");
+	printf("Gol. Darah\t: A\n");
+	printf("Alamat\t\t: Vila Tangerang Elok blok A 15 no. 7, Kab. Tangerang\n");
+	printf("Pekerjaan\t: Mahasiswa\n");
+	printf("Agama\t\t: Islam\n");
+	printf("Tinggi badan\t: 157 cm\n");
+	printf("Berat badan\t: 50 kg\n");
+	
+	
+	return 0;
+}
